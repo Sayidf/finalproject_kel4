5f664a69f142b5727dd61f16f9f552d880d05611
Sayidf <sayidfarhan2@gmail.com>
2022-11-17 07:14:21 +0700

    navbar styling

--- resources/views/back/header.blade.php
+++ resources/views/back/header.blade.php
@@ -44,7 +44,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="page-error-404.html">
+            <a href="{{ url('sesi/logout') }}">
               <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                 <polyline points="16 17 21 12 16 7"></polyline>
--- resources/views/landingpage/navigation.blade.php
+++ resources/views/landingpage/navigation.blade.php
@@ -1,40 +1,35 @@
 <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
-        <h1 class="logo me-auto me-lg-0"><a href="{{ url('/home') }}">NF Culinary</a></h1>
-        <nav id="navbar" class="navbar order-last order-lg-0">
-            <ul>
-                <li><a class="nav-link scrollto" href="{{ url('/home') }}">Home</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/menu') }}">Menu</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/specials') }}">Specials</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/chefs') }}">Chefs</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/gallery') }}">Gallery</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/contact') }}">Contact</a></li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
-        <div class="justify-content-center d-flex">
-            @if (Auth::check())
-                <a href="{{ route('reservasi.create') }}" class="book-a-table-btn d-none d-lg-flex">Book a table</a>
-                <span class="mt-2 mx-3">|</span>
-                <div class="nav-link mt-2 dropdown">
-                    <a class=" dropdown-toggle" href="#" style="color: white" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        {{ Auth::user()->fullname }}
-                    </a>
-
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                        <li><a class="dropdown-item" href="{{ url('sesi/logout') }}">Logout</a></li>
-
-                    </ul>
-                </div>
-                {{-- <a class="nav-link mt-2" href="{{ url('sesi/logout') }}"><i
-                        class='bx bx-log-out-circle'></i>&nbsp;Logout</a> --}}
-            @else
-                <a class="nav-link mt-2" href="{{ url('/sesi') }}"><i class='bx bx-log-in-circle'></i>
-                    &nbsp;Login</a>
-            @endif
-
-
-        </div>
+  <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+    <h1 class="logo me-auto me-lg-0"><a href="{{ url('/home') }}">NF Culinary</a></h1>
+    <nav id="navbar" class="navbar order-last order-lg-0">
+      <ul>
+        <li><a class="nav-link scrollto" href="{{ url('/home') }}">Home</a></li>
+        <li><a class="nav-link scrollto" href="{{ url('/menu') }}">Menu</a></li>
+        <li><a class="nav-link scrollto" href="{{ url('/specials') }}">Specials</a></li>
+        <li><a class="nav-link scrollto" href="{{ url('/chefs') }}">Chefs</a></li>
+        <li><a class="nav-link scrollto" href="{{ url('/gallery') }}">Gallery</a></li>
+        <li><a class="nav-link scrollto" href="{{ url('/contact') }}">Contact</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav>
+    <div class="justify-content-center d-flex">
+      @if (Auth::check())
+        <a href="{{ route('reservasi.create') }}" class="book-a-table-btn d-none d-lg-flex">Book a table</a>
+        <span class="mt-2 mx-3">|</span>
+				<div class="navbar navbar-user">
+					<ul>
+						<li class="dropdown"><a href="#"><span>{{ Auth::user()->fullname }}</span> <i class="bi bi-caret-down-fill"></i></a>
+        	    <ul>
+        	      <li><a href="{{ url('sesi/logout') }}">Logout</a></li>
+        	    </ul>
+        	  </li>
+					</ul>
+				</div>
+        {{-- <a class="nav-link mt-2" href="{{ url('sesi/logout') }}"><i
+                class='bx bx-log-out-circle'></i>&nbsp;Logout</a> --}}
+      @else
+        <a class="nav-link mt-2" href="{{ url('/sesi') }}"><i class='bx bx-log-in-circle'></i>&nbsp;Login</a>
+      @endif
     </div>
+  </div>
 </header>
