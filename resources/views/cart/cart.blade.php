16c5dceddda141f6cb08cfaf3867c83e22bdc73d
Sayidf <sayidfarhan2@gmail.com>
2022-12-03 06:17:10 +0700

    merge cart style (dinda)

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -1,62 +1,64 @@
 @extends('landingpage.index')
 @section('content')
-  <section id="login" class="login">
-    <div class="container" data-aos="fade-up">
-      <table class="table table-dark table-striped" id="cart">
-        <thead>
-          <tr>
-            <th class="text-center">Menu</th>
-            <th>Harga</th>
-            <th style="width: 8%">Quantity</th>
-            <th class="text-center">Subtotal</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          @php $total = 0 @endphp
-          @if (session('cart'))
-            @foreach (session('cart') as $id => $details)
-              @php $total += $details['harga'] * $details['quantity'] @endphp
-              <tr data-id="{{ $id }}">
-                <td data-th="Product">
-                  <div class="row">
-                    <div class="col-sm-3 hidden-xs"><img src="{{ url('/public/assets/img/menu') }}/{{ $details['foto'] }}" width="100" height="100" class="img-responsive" /></div>
-                    <div class="col-sm-9">
-                      <h4 class="text-start">{{ $details['nama'] }}</h4>
-                    </div>
-                  </div>
-                </td>
-                <td data-th="Price">Rp{{ $details['harga'] }}</td>
-                <td data-th="Quantity">
-                  <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity update-cart" />
-                </td>
-                <td data-th="Subtotal" class="text-center">Rp{{ $details['harga'] * $details['quantity'] }}
-                </td>
-                <td class="actions" data-th="">
-                  <button class="btn btn-danger btn-sm remove-from-cart" title="Delete"><i class="far fa-trash-alt"></i></button>
-                </td>
-              </tr>
-            @endforeach
-          @endif
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="5" class="text-end">
-              <h3><strong>Total {{ $total }}</strong></h3>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="5" class="text-end">
-              <a href="{{ url('/') }}" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a>
-              @if (Auth::check())
-                <button class="btn btn-success">Checkout</button>
-              @else
-                <a href="{{ url('/login') }}" class="btn btn-success">Checkout</a>
-              @endif
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+  <section id="login" class="login" style="background: #1a1814">
+    <div class="container">
+      <div data-aos="fade-up">
+        <table class="table-responsive" id="cart">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Menu</th>
+              <th>Harga</th>
+              <th>Quantity</td>
+              <th>Subtotal</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            @php $total = 0 @endphp
+            @if (session('cart'))
+              @foreach (session('cart') as $id => $details)
+                @php $total += $details['harga'] * $details['quantity'] @endphp
+                <tr data-id="{{ $id }}">
+                  <td><img src="{{ url('/public/assets/img/menu') }}/{{ $details['foto'] }}" class="img-responsive" /></td>
+                  <td>
+                    {{ $details['nama'] }}
+                  </td>
+                  <td>Rp {{ $details['harga'] }}</td>
+                  <td>
+                    <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity update-cart" />
+                  </td>
+                  <td data-th="Subtotal">Rp {{ $details['harga'] * $details['quantity'] }}
+                  </td>
+                  <td class="actions" data-th="">
+                    <button class="btn btn-danger btn-sm remove-from-cart" title="Delete"><i
+                        class="far fa-trash-alt"></i></button>
+                  </td>
+                </tr>
+              @endforeach
+            @endif
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="6" class="text-end">
+                Total: <span>
+                  Rp {{ $total }}
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="6" class="text-end">
+                {{-- <a href="{{ url('/') }}" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a> --}}
+                @if (Auth::check())
+                  <button class="btn btn-success">Checkout</button>
+                @else
+                  <a href="{{ url('/login') }}" class="btn btn-success">Checkout</a>
+                @endif
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   </section>
 @endsection
