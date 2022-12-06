ab1d63ec25c7bf7d4c6399615bbbe425b4cc9932
Sayidf <sayidfarhan2@gmail.com>
2022-12-07 06:08:09 +0700

    fix cart design for mobile

--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -3,11 +3,6 @@
 <!--======= Login Section =======-->
 <section id="login" class="login">
   <div class="container" data-aos="fade-up">
-    @if ($message = Session::get('success'))
-      <div class="alert alert-success">
-        <p>{{ $message }}</p>
-      </div>
-    @endif
     @if ($errors->any())
       <div class="alert alert-danger">
         <ul>
