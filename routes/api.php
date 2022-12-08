4c1460351160857537f277c4e8a7e670be6dd8f3
Rizki123 <rizki99sofyan@gmail.com>
2022-12-08 23:31:33 +0700

    new again

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,4 @@ Route::post('/reservasi-create', [ReservasisController::class, 'store']);
 
 Route::get('/menu', [MenuController::class, 'index']);
 Route::get('/menu/{id}', [MenuController::class, 'show']);
+Route::get('/menu-create', [MenuController::class, 'store']);
